6351529fea8563f9bfeba6d79944dee5cec5bdc4
Nathan Betts <nathanbetts@hotmail.co.uk>
2023-10-31 21:39:04 +0000

    split out express app and server

---
+++ src/api/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import * as authApi from './auth';
+import * as userApi from './user';
+import { validateRequest } from './validation';
+import * as validators from '../validation/validators';
+
+export const app = express();
+
+app.use(bodyParser.json());
+app.post('/session', validators.logInValidator, [validateRequest, authApi.logIn, authApi.createSession]);
+app.delete('/session', validators.logOutValidator, [validateRequest, authApi.logOut]);
+app.put('/session', validators.refreshSessionValidator, [validateRequest, authApi.refreshSession, authApi.createSession]);
+app.post('/user', validators.registerValidator, [validateRequest, userApi.register, authApi.logIn, authApi.createSession]);
+app.get('/user', [authApi.authenticateUser, userApi.getUserDetails]);
+app.put('/user', validators.updateUserValidator, [validateRequest, authApi.authenticateUser, userApi.updateUserDetails]);
+app.put('/user/password', validators.updatePasswordValidator, [validateRequest, authApi.authenticateUser, userApi.updatePassword, authApi.createSession]);
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import * as authApi from './api/auth';
-import * as userApi from './api/user';
-import { validateRequest } from './api/validation';
-import * as validators from './validation/validators';
+import { app } from './api/app';
 
 console.log('Server initializing...');
-export const server = express();
 const port = 3000;
 
-server.use(bodyParser.json());
-server.post('/session', validators.logInValidator, [validateRequest, authApi.logIn, authApi.createSession]);
-server.delete('/session', validators.logOutValidator, [validateRequest, authApi.logOut]);
-server.put('/session', validators.refreshSessionValidator, [validateRequest, authApi.refreshSession, authApi.createSession]);
-server.post('/user', validators.registerValidator, [validateRequest, userApi.register, authApi.logIn, authApi.createSession]);
-server.get('/user', [authApi.authenticateUser, userApi.getUserDetails]);
-server.put('/user', validators.updateUserValidator, [validateRequest, authApi.authenticateUser, userApi.updateUserDetails]);
-server.put('/user/password', validators.updatePasswordValidator, [validateRequest, authApi.authenticateUser, userApi.updatePassword, authApi.createSession]);
-server.listen(port, () => console.log('Server listening on port', port));
+app.listen(port, () => {
+  console.log('Server listening on port', port);
+});
